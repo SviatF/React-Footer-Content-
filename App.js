7b800650d51e2f5dca089588926d62ba13bf75ef
SviatF <85065470+SviatF@users.noreply.github.com>
2022-04-01 12:51:52 +0300

    Папка else-func-toHome-page мої завд на фреймворк

---
+++ App.js
@@ -0,0 +1,14 @@
+import Footer from "./else-func-toHome-page/footer";
+import Main from "./else-func-toHome-page/main-home-page";
+
+function App() {
+  return (
+    <div className="main">
+    
+      <Main/>
+      < Footer/>  
+    </div>
+  );
+}
+
+export default App;
